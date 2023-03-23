420c239dc40b5f162a036098e850efaa4c04b072
ixyhoo <ixyhoo@outlook.fr>
2023-03-23 17:58:53 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+get "ping" => "application#ping"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
